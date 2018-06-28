cf62a70433be3fcc2e1adb02bc676a06e720556f
NguyenHaiPhong <thelivingdeath.n2h.destroyer@gmail.com>
2018-06-28 11:09:49 +0700

    commit

--- Session02/homework/serious_exercises_3.d.py
+++ Session02/homework/serious_exercises_3.d.py
@@ -1,18 +1,22 @@
 # i.
-print("The first pattern: 9 x 9 numbers (multiplication table).")
-print()
-for i in range(1, 10):
-    for j in range (1, 10):
-        if (i % 2 == 0) and (j % 2 == 0):
-            print("0", " ", end = "")     
+print("The first pattern: 10 x 10 1’s and 0’s, consecutively.")
+a = int(10)
+for i in range(0, a):
+    for j in range(i + 1, a + 1):
+        if (j % 2 == 0):
+            print("0", " ", end = " ")
         else: 
-            print("1")
+            print("1"," ", end = " ")
     print()
+    a = a + 1
 # ii.
-# print("The second pattern: Ask user to enter a number n, then print n x n numbers, following multiplication table pattern.")
-# print()
-# number = int(input("Enter a number: "))
-# for i in range(1, number + 1):
-#     for j in range (1, number + 1):
-#         print(i * j, " ",end = " ")
-#     print()
+print("The second pattern: Ask users to enter a number n, then print n x n 1’s and 0’s, consecutively.")
+b = int(input(print("Enter a nubmer: ")))
+for i in range(b):
+    for j in range(i + 1, b + 1):
+        if (j % 2 == 0):
+            print("0", " ", end = " ")
+        else: 
+            print("1"," ", end = " ")
+    print()
+    b = b + 1 
